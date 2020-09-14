e8cd7bd68be48dc9ac081cb56f09ac2997439bd2
glenray <40870346+glenray@users.noreply.github.com>
2020-09-14 14:57:34 -0400

    Competes right and left arrow moving through stack history except for en passant

--- gui.py
+++ gui.py
@@ -42,7 +42,6 @@ class GUI:
 	def printCurrentBoard(self):
 		self.pieceTkImg = []
 		self.canvas.update()
-		dim = int(round(self.canvas.winfo_width()/8))
 		self.canvas.delete('piece')
 		# piece_map returns a dictionary where 
 		# key is the square number and value is a piece object
@@ -51,15 +50,20 @@ class GUI:
 		# home to the white light squared rook.
 		pm = self.board.piece_map()
 		for key in pm:
-			self.putImage(key, dim)
+			self.putImage(key)
 
-	def putImage(self, square, dim):
+	def putImage(self, square):
+		dim = int(round(self.canvas.winfo_width()/8))
 		coords = self.canvas.coords(chess.square_name(square))
 		piece = self.board.piece_at(square) 
 		im = self.pieceImg[piece.symbol()]
 		im = im.resize((int(dim), int(dim)), Image.LANCZOS)
 		self.pieceTkImg.append(ImageTk.PhotoImage(image=im))
 		self.canvas.create_image((coords[0], coords[1]), image=self.pieceTkImg[-1], anchor='nw', tag='piece')
+
+	def deletePieceImage(self, sq):
+		piece = self.getPieceObj(sq)
+		self.canvas.delete(piece)
 
 	# populate dictonary containing png images of pieces
 	def grabPieceImages(self):
@@ -76,8 +80,8 @@ class GUI:
 		self.root.title("Glen's Chess Analysis Wizard")
 		self.root.bind("<Escape>", lambda e: self.root.destroy())
 		self.root.geometry(f"{self.boardSize*2}x{self.boardSize*2}")
-		self.root.bind('<Right>', self.moveForw)
-		self.root.bind('<Left>', self.moveBack)
+		self.root.bind('<Right>', self.navMoveStack)
+		self.root.bind('<Left>', self.navMoveStack)
 
 		# Fonts and Styling
 		# print(font.families())	# prints available font families
@@ -110,64 +114,104 @@ class GUI:
 		# Reverse Board Button
 		self.reverseBoardButton = tk.Button(self.buttonBarFrame, buttonOptions, text="Reverse Board", command=self.reverseBoard)
 		self.reverseBoardButton.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
-
 		
 		# Add widgets to paned window
 		self.pWindow.add(self.boardFrame, weight=1)
 		self.pWindow.add(self.controlFrame, weight=1)
 
-	def moveForw(self, e):
-		print("right key pressed")
+	
+	'''
+	Return coordinates of a canvas rectangle object
+	@ sq obj a chess.square object
+	@ return a 4 element tuple containing the 
+	x,y locations of the upper left and lower right points of the square
+	'''
+	def getSqCoords(self, sq):
+		sqName = chess.square_name(sq)
+		return self.canvas.coords(sqName)
 
+	'''
+	Return canvas image object on a square
+	@ sq obj a chess.square object with a piece on it
+	return obj a canvas object image representing the piece
+	'''
+	def getPieceObj(self, sq):
+		sqCoords = self.getSqCoords(sq)
+		x = (sqCoords[0]+sqCoords[2])/2
+		y = (sqCoords[1]+sqCoords[3])/2
+		return self.canvas.find_closest(x, y)		
+
+	'''
+	Move a piece from on sq to another
+	@ fromSq obj chess.square object with piece on it
+	@ toSq obj chess.square object to relocate piece
+	'''
 	def moveCanvasPiece(self, fromSq, toSq):
-		# get the from and to squares of the move
-		fromSqName = chess.square_name(fromSq)
-		toSqName = chess.square_name(toSq)
-		fromPieceName = self.board.piece_at(fromSq)
-		toPieceName = self.board.piece_at(toSq)
-		fromSqCoords = self.canvas.coords(fromSqName)
-		toSqCoords = self.canvas.coords(toSqName)
-
-		# find the piece canvas object to move
-		x = (fromSqCoords[0]+fromSqCoords[2])/2
-		y = (fromSqCoords[1]+fromSqCoords[3])/2
-		obj = self.canvas.find_closest(x, y)
-
+		fromSqCoords = self.getSqCoords(fromSq)
+		toSqCoords = self.getSqCoords(toSq)
+		obj = self.getPieceObj(fromSq)
 		# find the distance between the from and to coordinates
 		dx = toSqCoords[0]-fromSqCoords[0]
 		dy = toSqCoords[1]-fromSqCoords[1]
 
 		self.canvas.move(obj, dx, dy)		
 
-	def moveBack(self, e):
-		if len(self.board.move_stack) == 0 : return
-		previousMove = self.board.pop()
-		self.moveHistory.append(previousMove)
+	def navMoveStack(self, e):
+		# moving back in game
+		if e.keysym == 'Left':
+			# quit if at first move
+			if len(self.board.move_stack) == 0 : return
+			previousMove = self.board.pop()
+			# These test must be done after the move is popped off the move stack
+			# See else clause explanation
+			isCaptureMove = self.board.is_capture(previousMove)
+			isCastling = self.board.is_castling(previousMove)
+			isKingSideCastling = self.board.is_kingside_castling(previousMove)
+			isEnPassant = self.board.is_en_passant(previousMove)
+			self.moveHistory.append(previousMove)
+			# moving backward, the move to square is the location of the piece and the from is where it needs to be returned to.
+			self.moveCanvasPiece(previousMove.to_square, previousMove.from_square)
+			captureSquare, captureFunction = previousMove.to_square, self.putImage
+		# moving forward in game
+		else:
+			# quit if at last move
+			if len(self.moveHistory) == 0 : return
+			previousMove = self.moveHistory.pop()
+			# These tests must be done before the move is pushed onto the board
+			# These test return true only for valid moves
+			# The moves are valid only before the board position is updated
+			isCastling = self.board.is_castling(previousMove)
+			isKingSideCastling = self.board.is_kingside_castling(previousMove)
+			isCaptureMove = self.board.is_capture(previousMove)
+			isEnPassant = self.board.is_en_passant(previousMove)
+			self.board.push(previousMove)
+			self.moveCanvasPiece(previousMove.from_square, previousMove.to_square)
+			captureSquare, captureFunction = previousMove.to_square, self.deletePieceImage
 
-		# moving backward, the move to square is the location of the piece and the from is where it needs to be returned to.
-		self.moveCanvasPiece(previousMove.to_square, previousMove.from_square)
-
-		# For capture moves, replace the taken piece on the board
-		# Moving foward in move history, this will need to be remove piece from board
-		isCaptureMove = self.board.san(previousMove).count('x')
+		# Moving backward in the move history, captured pieces need to reappear
+		# Moving foward, captured pieces need to be removed.
+		# capture function is assigned based on which arrow key was pressed
 		if isCaptureMove:
-			dim = int(round(self.canvas.winfo_width()/8))
-			self.putImage(previousMove.to_square, dim)
+			captureFunction(captureSquare)
 
-		# For castling, put rook back to original location
-		if self.board.is_castling(previousMove):
-			rank = chess.square_rank(previousMove.to_square) # int
-			if self.board.is_kingside_castling(previousMove):
-				fromFile, toFile  = 5,7
-			else:
-				fromFile, toFile  = 3,0
-
+		# For castling, the king has already been moved above,
+		# but the rook still needs to be put where it belongs
+		if isCastling:
+			rookToSq = previousMove.to_square if e.keysym == 'Left' else previousMove.from_square
+			rank = chess.square_rank(rookToSq)
+			# set castled rook files depending which side of the board
+			fromFile, toFile  = (5,7) if isKingSideCastling else (3,0)
 			fromSq = chess.square(fromFile,rank)
 			toSq = chess.square(toFile,rank)
-			self.moveCanvasPiece(fromSq, toSq)
+
+			if e.keysym == 'Left':
+				self.moveCanvasPiece(fromSq, toSq) # uncastle rook
+			else:
+				self.moveCanvasPiece(toSq, fromSq) # castle rook
+
 
 		# For En Passant Capture
-		if self.board.is_en_passant(previousMove):
+		if isEnPassant:
 			print("En Passant")
 
 	# create 64 rectangles to be sized and positioned later
