046b62a152487a1ca01b567ade78ca444de37f8b
glenray <40870346+glenray@users.noreply.github.com>
2020-09-13 23:02:14 -0400

    Adds functionality to move backward in the move stack

--- gui.py
+++ gui.py
@@ -21,6 +21,7 @@ class GUI:
 		# They are stored here only to protect them from garbage collection.
 		self.pieceTkImg = []
 		self.whiteSouth = True	# True: white pieces on south side of board; False reverse
+		self.moveHistory = []	# populate with newer moves when going back in the move stack
 
 	def setup(self):
 		self.createWidgets()
@@ -29,20 +30,20 @@ class GUI:
 		self.grabPieceImages()
 		self.loadGame('pgn/blind.pgn')
 		# self.setStartPos()
+		self.printCurrentBoard()
 		self.root.mainloop()
 
 	def loadGame(self, path):
 		self.board = self.loadPgnFile(path)
-		self.printCurrentBoard()
 
 	def setStartPos(self):
 		self.board.reset()
-		self.printCurrentBoard()
 
 	def printCurrentBoard(self):
 		self.pieceTkImg = []
 		self.canvas.update()
 		dim = int(round(self.canvas.winfo_width()/8))
+		self.canvas.delete('piece')
 		# piece_map returns a dictionary where 
 		# key is the square number and value is a piece object
 		# piece.symbol() returns the letter description for the piece, eg P or p  
@@ -58,7 +59,7 @@ class GUI:
 		im = self.pieceImg[piece.symbol()]
 		im = im.resize((int(dim), int(dim)), Image.LANCZOS)
 		self.pieceTkImg.append(ImageTk.PhotoImage(image=im))
-		self.canvas.create_image((coords[0], coords[1]), image=self.pieceTkImg[-1], anchor='nw')
+		self.canvas.create_image((coords[0], coords[1]), image=self.pieceTkImg[-1], anchor='nw', tag='piece')
 
 	# populate dictonary containing png images of pieces
 	def grabPieceImages(self):
@@ -75,6 +76,8 @@ class GUI:
 		self.root.title("Glen's Chess Analysis Wizard")
 		self.root.bind("<Escape>", lambda e: self.root.destroy())
 		self.root.geometry(f"{self.boardSize*2}x{self.boardSize*2}")
+		self.root.bind('<Right>', self.moveForw)
+		self.root.bind('<Left>', self.moveBack)
 
 		# Fonts and Styling
 		# print(font.families())	# prints available font families
@@ -112,6 +115,60 @@ class GUI:
 		# Add widgets to paned window
 		self.pWindow.add(self.boardFrame, weight=1)
 		self.pWindow.add(self.controlFrame, weight=1)
+
+	def moveForw(self, e):
+		print("right key pressed")
+
+	def moveCanvasPiece(self, fromSq, toSq):
+		# get the from and to squares of the move
+		fromSqName = chess.square_name(fromSq)
+		toSqName = chess.square_name(toSq)
+		fromPieceName = self.board.piece_at(fromSq)
+		toPieceName = self.board.piece_at(toSq)
+		fromSqCoords = self.canvas.coords(fromSqName)
+		toSqCoords = self.canvas.coords(toSqName)
+
+		# find the piece canvas object to move
+		x = (fromSqCoords[0]+fromSqCoords[2])/2
+		y = (fromSqCoords[1]+fromSqCoords[3])/2
+		obj = self.canvas.find_closest(x, y)
+
+		# find the distance between the from and to coordinates
+		dx = toSqCoords[0]-fromSqCoords[0]
+		dy = toSqCoords[1]-fromSqCoords[1]
+
+		self.canvas.move(obj, dx, dy)		
+
+	def moveBack(self, e):
+		if len(self.board.move_stack) == 0 : return
+		previousMove = self.board.pop()
+		self.moveHistory.append(previousMove)
+
+		# moving backward, the move to square is the location of the piece and the from is where it needs to be returned to.
+		self.moveCanvasPiece(previousMove.to_square, previousMove.from_square)
+
+		# For capture moves, replace the taken piece on the board
+		# Moving foward in move history, this will need to be remove piece from board
+		isCaptureMove = self.board.san(previousMove).count('x')
+		if isCaptureMove:
+			dim = int(round(self.canvas.winfo_width()/8))
+			self.putImage(previousMove.to_square, dim)
+
+		# For castling, put rook back to original location
+		if self.board.is_castling(previousMove):
+			rank = chess.square_rank(previousMove.to_square) # int
+			if self.board.is_kingside_castling(previousMove):
+				fromFile, toFile  = 5,7
+			else:
+				fromFile, toFile  = 3,0
+
+			fromSq = chess.square(fromFile,rank)
+			toSq = chess.square(toFile,rank)
+			self.moveCanvasPiece(fromSq, toSq)
+
+		# For En Passant Capture
+		if self.board.is_en_passant(previousMove):
+			print("En Passant")
 
 	# create 64 rectangles to be sized and positioned later
 	def createSquares(self):
@@ -165,7 +222,7 @@ class GUI:
 		self.positionSquares()
 		self.printCurrentBoard()
 
-	def reverseBoard(self, e=None):
+	def reverseBoard(self):
 		self.whiteSouth = not self.whiteSouth
 		self.positionSquares()
 		self.printCurrentBoard()
