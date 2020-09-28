d24744c8e556b06eed8b61a396ca2be0109ab1ff
glenray <40870346+glenray@users.noreply.github.com>
2020-09-28 19:49:02 -0400

    updates pgnReader class to search headers without an exact match, i.e. 'Karpov' will return if header contains 'Karpov, Anatoly

--- pgnReader.py
+++ pgnReader.py
@@ -1,6 +1,18 @@
 import chess.pgn
 
 class pgnReader:
+	def __init__(self, pgnPath):
+		self.pgnPath = pgnPath
+		self.fileObj = open(self.pgnPath)
+
+	def getGameList(self, searchParams):
+		gameList = []
+		offsets = self.searchGames(searchParams)
+		for offset in offsets:
+			gameList.append(self.getOneGame(offset))
+		return gameList
+
+
 	# find games in pgn file based on search parameters
 	# @searchParams array of dictionaries
 	# @returns array of offsets in pgn file where games are located
@@ -15,36 +27,46 @@ class pgnReader:
 	# will show games where Deep Blue won with either color
 	def searchGames(self, searchParams):
 		offsets = []
-		pgn = open('pgn/Kasparovs_Games.pgn')
+		self.fileObj.seek(0)
 		while True:
-			offset = pgn.tell()
-			headers = chess.pgn.read_headers(pgn)
+			offset = self.fileObj.tell()
+			headers = chess.pgn.read_headers(self.fileObj)
 			if headers == None: break
 			for search in searchParams:
 				if self.isSubSet(headers, search):
 					offsets.append(offset)
-				else:
-					continue
 		return offsets			
 
+	# get game at offset
 	def getOneGame(self, offset):
-		pgn = open('pgn/Kasparovs_Games.pgn')
-		pgn.seek(offset)
-		return chess.pgn.read_game(pgn)
+		self.fileObj.seek(offset)
+		return chess.pgn.read_game(self.fileObj)
 
 	# Are all elements of subset in set?
 	def isSubSet(self, set, subset):
-		return all(set.get(key, None) == val for key, val in subset.items())
+		# value in subset must exactly equal set
+		# return all(set.get(key, None) == val for key, val in subset.items())
+		# value in subset must be somewhere in set
+		return all(val in set.get(key, None) for key, val in subset.items())
 
 if __name__ == '__main__':
-	search = [
+	dbWins = [
 		{'White' : 'Deep Blue', 'Result': '1-0'}, 
 		{'Black' : 'Deep Blue', 'Result': '0-1'}
 	]
-
-	reader = pgnReader()
-	offsets = reader.searchGames(search)
-	for offset in offsets:
-		game = reader.getOneGame(offset)
-		print(game)
-	
+	dbWhiteTies = [
+		{'White' : 'Deep Blue', 'Result': '1/2'} 
+	]
+	dbGames = [
+		{'White':'Deep Blue'},
+		{'Black':'Deep Blue'}
+	]
+	karpov = [
+		{'White':'Tal'},
+		{'Black':'Tal'}
+	]
+	reader = pgnReader('pgn/Kasparovs_Games.pgn')
+	games = reader.getGameList(dbWhiteTies)
+	for game in games:
+		print(f"{game.headers['Site']} {game.headers['White']} v. {game.headers['Black']} {game.headers['Result']}")
+	print(len(games))
