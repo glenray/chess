a9369b20810e29b486249b0865f132c8645d43d0
glenray <40870346+glenray@users.noreply.github.com>
2020-11-16 16:16:09 -0500

    Adds settings fields to blunder check window

--- blunderCheck.py
+++ blunderCheck.py
@@ -20,6 +20,13 @@ class blunderCheck():
 		self.engines = {
 			'Stockfish' : "C:/Users/Glen/Documents/python/stockfish/bin/stockfish_20090216_x64_bmi2.exe"
 		}
+		self.defaults = {
+			'threshold'	: '50',
+			'depth'	: '15',
+			'time'	: '15',
+			'begMove'	: '1',
+			'endMove'	: ''
+		}
 
 	def analyzePosition(self, board, limit):
 		engine=chess.engine.SimpleEngine.popen_uci(self.engines['Stockfish'])
@@ -43,7 +50,7 @@ class blunderCheck():
 		textFont = font.Font(family="Courier", size=14)
 
 		buttonOptions = {"pady":5, "padx":5, "overrelief":'sunken', "font":buttonFont}
-		labelOptions = {"pady": 10, "font": labelFont}
+		titleOptions = {"pady": 10, "font": labelFont}
 
 		self.blWindow = tk.Toplevel(self.gui.root)
 		self.blWindow.title("Blunder Check")
@@ -52,21 +59,25 @@ class blunderCheck():
 		# make window modal
 		self.blWindow.grab_set()
 		self.blWindow.grid_columnconfigure(0, weight=1)
-		self.blWindow.grid_columnconfigure(1, weight=0)
-		# self.blWindow.bind('<Return>', self.start)
-
-		self.label = tk.Label(self.blWindow, text="Blunder Check")
-		self.label.configure(labelOptions)
-		self.label.grid(row=0, column=0)
+		self.blWindow.grid_columnconfigure(1, weight=2)
+		self.blWindow.grid_columnconfigure(2, weight=3)
 
 		self.blText = tk.Text(self.blWindow, width=80, font=textFont, padx=10, pady=5)
-		self.blText.grid(row=1, column=0, sticky='nsew', padx=10)
+		self.blText.grid(row=1, column=0, columnspan=2, sticky='nsew', padx=10)
+
+		self.label = tk.Label(self.blWindow, text="Blunder Check")
+		self.label.configure(titleOptions)
+		self.label.grid(row=0, column=0, columnspan=2)
 
 		self.buttonFrame = tk.Frame(self.blWindow)
+		self.buttonFrame.columnconfigure(0, weight=0)
 		self.buttonFrame.grid(row=2, column=0, sticky='nsew')
-		self.buttonFrame.columnconfigure(0, weight=1)
-		self.buttonFrame.columnconfigure(1, weight=1)
-		self.buttonFrame.rowconfigure(0, weight=1)
+
+		self.settingFrame = tk.Frame(self.blWindow)
+		self.settingFrame.columnconfigure(1, weight=1)
+		self.settingFrame.columnconfigure(2, weight=2)
+		self.settingFrame.grid(row=2, column=1, sticky='nsew')
+
 
 		self.runButton = tk.Button(self.buttonFrame, text="Run", command=self.openBlCheck)
 		self.runButton.configure(buttonOptions)
@@ -75,17 +86,65 @@ class blunderCheck():
 
 		self.cancelButton = tk.Button(self.buttonFrame, text="Cancel", command=blunderOff)
 		self.cancelButton.configure(buttonOptions)
-		self.cancelButton.grid(row=0, column=1, padx=5, pady=5, sticky='ew')
+		self.cancelButton.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
 
-		# self.runButton.rowconfigure(, weight=1)
+		self.depthLabel = tk.Label(self.settingFrame, text="Depth")
+		self.depthLabel.grid(row=0, column=1, sticky='e')
+		self.depthEntry = tk.Entry(self.settingFrame)
+		self.depthEntry.grid(row=0, column=2, sticky='w')
+		self.depthEntry.insert('0', self.defaults['depth'])
+
+		self.timeLabel = tk.Label(self.settingFrame, text="Time")
+		self.timeLabel.grid(row=1, column=1, sticky='e')
+		self.timeEntry = tk.Entry(self.settingFrame)
+		self.timeEntry.grid(row=1, column=2, sticky='w')
+		self.timeEntry.insert('0', self.defaults['time'])
+
+		self.threshLabel = tk.Label(self.settingFrame, text="Threshold")
+		self.threshLabel.grid(row=2, column=1, sticky='e')
+		self.threshEntry = tk.Entry(self.settingFrame)
+		self.threshEntry.grid(row=2, column=2, sticky='w')
+		self.threshEntry.insert('0', self.defaults['threshold'])
+
+		self.begMoveLabel = tk.Label(self.settingFrame, text="Begin Move")
+		self.begMoveLabel.grid(row=3, column=1, sticky='e')
+		self.begMoveEntry = tk.Entry(self.settingFrame)
+		self.begMoveEntry.grid(row=3, column=2, sticky='w')
+		self.begMoveEntry.insert('0', self.defaults['begMove'])
+
+		self.endMoveLabel = tk.Label(self.settingFrame, text="End Move")
+		self.endMoveLabel.grid(row=4, column=1, sticky='e')
+		self.endMoveEntry = tk.Entry(self.settingFrame)
+		self.endMoveEntry.grid(row=4, column=2, sticky='w')
+		self.endMoveEntry.insert('0', self.defaults['endMove'])		
 
 	def openBlCheck(self, e=None):
 		self.runButton.configure(state='disabled')
+		time = self.timeEntry.get()
+		depth = self.depthEntry.get()
+		thresh = self.threshEntry.get()
+		begMove = self.begMoveEntry.get()
+		endMove = self.endMoveEntry.get()
+
+		time = int(time) if self.isInteger(time) else None
+		depth = int(depth) if self.isInteger(depth) else None
+		thresh = int(thresh) if self.isInteger(thresh) else None
+		begMove = int(begMove) if self.isInteger(begMove) else 1
+		endMove = int(endMove) if self.isInteger(endMove) else None
+
 		threading.Thread(
 			target=self.blunderChk, 
-			kwargs=dict(depth=15, blunderThresh=50),
+			kwargs=dict(depth=depth, time=time, blunderThresh=thresh, begMove=begMove, endMove=endMove),
 			name="Blunder Checker",
 			daemon=True).start()
+
+	def isInteger(self, n):
+		try:
+			float(n)
+		except ValueError:
+			return False
+		else:
+			return float(n).is_integer()
 
 	# runs in a separate thread
 	def blunderChk(self, begMove=1, endMove=None, blunderThresh=50, depth=5, time=None):
@@ -148,7 +207,13 @@ class blunderCheck():
 				blunderWarning = ''
 
 			if isBlunder:
-				line = node.parent.add_line(saveInfo['pv'], starting_comment=blunderWarning)
+				moveVar = [x.move for x in node.parent.variations]
+				# don't add line if the parent variation already has the same
+				# variation
+				if(saveInfo['pv'][0] in moveVar):
+					blunderWarning = f"{moveTxt} Repeat! \n"
+				else:
+					line = node.parent.add_line(saveInfo['pv'], starting_comment=blunderWarning)
 				self.blText.insert('end', f"{moveTxt} {blunderWarning}\n")
 			else:
 				self.blText.insert('end', moveTxt+"\n")
